4e6dd6536c153fe32d48d04268af645188857090
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-02-26 14:44:10 -0400

    Use 4 threads and update dependencies

--- Android.mk
+++ Android.mk
@@ -22,9 +22,9 @@ rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
 
 # Creating prebuilt for dependency: beatsaber-hook - version: 1.0.10
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_1_0_12
+LOCAL_MODULE := beatsaber-hook_1_1_0
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_0_12.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_1_0.so
 LOCAL_CPP_FEATURES += exceptions
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: bs-utils - version: 0.5.8
@@ -65,7 +65,7 @@ LOCAL_SRC_FILES += $(call rwildcard,src/,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_0_12
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_1_0
 LOCAL_SHARED_LIBRARIES += bs-utils
 LOCAL_SHARED_LIBRARIES += codegen_0_6_2
 LOCAL_SHARED_LIBRARIES += custom-types
