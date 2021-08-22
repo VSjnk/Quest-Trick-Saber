8f8a230aad2fc59c90ac57da2c06e54a3e55c425
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-08-21 21:56:04 -0400

    Version bump to 0.3.7

--- Android.mk
+++ Android.mk
@@ -71,7 +71,7 @@ LOCAL_SHARED_LIBRARIES += codegen_0_12_5
 LOCAL_SHARED_LIBRARIES += custom-types
 LOCAL_SHARED_LIBRARIES += questui
 LOCAL_LDLIBS += -llog
-LOCAL_CFLAGS += -I'extern/libil2cpp/il2cpp/libil2cpp' -DID='"TrickSaber"' -DVERSION='"0.3.6"' -I'./shared' -I'./extern' -isystem'extern/codegen/include' -O2
+LOCAL_CFLAGS += -I'extern/libil2cpp/il2cpp/libil2cpp' -DID='"TrickSaber"' -DVERSION='"0.3.7"' -I'./shared' -I'./extern' -isystem'extern/codegen/include' -O2
 LOCAL_CPPFLAGS += -std=c++2a
 LOCAL_C_INCLUDES += ./include ./src
 include $(BUILD_SHARED_LIBRARY)
--- buildQmod.ps1
+++ buildQmod.ps1
@@ -3,6 +3,6 @@
 
 if ($?) {
     Compress-Archive -Path "./mod.json", "./libs/arm64-v8a/libTrickSaber.so", "./libs/arm64-v8a/libbs-utils.so", "./libs/arm64-v8a/libbeatsaber-hook_2_2_5.so" -DestinationPath "./TrickSaber_v0.1.0.zip" -Update
-    Remove-Item "./TrickSaber_v0.3.6.qmod"
-    Rename-Item "./TrickSaber_v0.1.0.zip" "./TrickSaber_v0.3.6.qmod"
+    Remove-Item "./TrickSaber_v0.3.7.qmod"
+    Rename-Item "./TrickSaber_v0.1.0.zip" "./TrickSaber_v0.3.7.qmod"
 }
