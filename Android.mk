2d5098c5585ccd9340ded3c8f2a7488518d72a82
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-09-05 18:44:59 -0400

    Readying for release 0.3.8

--- Android.mk
+++ Android.mk
@@ -33,11 +33,11 @@ LOCAL_MODULE := bs-utils
 LOCAL_EXPORT_C_INCLUDES := extern/bs-utils
 LOCAL_SRC_FILES := extern/libbs-utils.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: codegen - version: 0.12.5
+# Creating prebuilt for dependency: codegen - version: 0.13.0
 include $(CLEAR_VARS)
-LOCAL_MODULE := codegen_0_12_5
+LOCAL_MODULE := codegen_0_13_0
 LOCAL_EXPORT_C_INCLUDES := extern/codegen
-LOCAL_SRC_FILES := extern/libcodegen_0_12_5.so
+LOCAL_SRC_FILES := extern/libcodegen_0_13_0.so
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: modloader - version: 1.2.3
 include $(CLEAR_VARS)
@@ -45,24 +45,30 @@ LOCAL_MODULE := modloader
 LOCAL_EXPORT_C_INCLUDES := extern/modloader
 LOCAL_SRC_FILES := extern/libmodloader.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: custom-types - version: 0.12.6
+# Creating prebuilt for dependency: custom-types - version: 0.12.7
 include $(CLEAR_VARS)
 LOCAL_MODULE := custom-types
 LOCAL_EXPORT_C_INCLUDES := extern/custom-types
 LOCAL_SRC_FILES := extern/libcustom-types.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: questui - version: 0.10.2
+# Creating prebuilt for dependency: questui - version: 0.11.0
 include $(CLEAR_VARS)
 LOCAL_MODULE := questui
 LOCAL_EXPORT_C_INCLUDES := extern/questui
 LOCAL_SRC_FILES := extern/libquestui.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: questui_components - version: 0.1.3
+# Creating prebuilt for dependency: questui_components - version: 0.1.5
 include $(CLEAR_VARS)
 LOCAL_MODULE := questui_components
 LOCAL_EXPORT_C_INCLUDES := extern/questui_components
 LOCAL_SRC_FILES := extern/libquestui_components.so
 include $(PREBUILT_SHARED_LIBRARY)
+# Creating prebuilt for dependency: libcurl - version: 7.78.0
+include $(CLEAR_VARS)
+LOCAL_MODULE := curl
+LOCAL_EXPORT_C_INCLUDES := extern/libcurlf
+LOCAL_SRC_FILES := extern/libcurl.a
+include $(PREBUILT_STATIC_LIBRARY)
 
 
 include $(CLEAR_VARS)
@@ -73,12 +79,13 @@ LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
 LOCAL_SHARED_LIBRARIES += beatsaber-hook_2_3_0
 LOCAL_SHARED_LIBRARIES += bs-utils
-LOCAL_SHARED_LIBRARIES += codegen_0_12_5
+LOCAL_SHARED_LIBRARIES += codegen_0_13_0
 LOCAL_SHARED_LIBRARIES += custom-types
 LOCAL_SHARED_LIBRARIES += questui
 LOCAL_SHARED_LIBRARIES += questui_components
+LOCAL_STATIC_LIBRARIES += curl
 LOCAL_LDLIBS += -llog
-LOCAL_CFLAGS += -I'extern/libil2cpp/il2cpp/libil2cpp' -DID='"TrickSaber"' -DVERSION='"0.3.7"' -I'./shared' -I'./extern' -I'./extern/questui_components/shared' -isystem'extern/codegen/include' -O2
+LOCAL_CFLAGS += -I'extern/libil2cpp/il2cpp/libil2cpp' -DID='"TrickSaber"' -DVERSION='"0.3.8"' -I'./shared' -I'./extern' -I'./extern/questui_components/shared' -isystem'extern/codegen/include' -O2
 LOCAL_CPPFLAGS += -std=c++2a
 LOCAL_C_INCLUDES += ./include ./src
 include $(BUILD_SHARED_LIBRARY)
